6975bb061017e24f5236013b0f715caed8bd07e8
Barbodnasimi <barbod.nasimi.2006@gmail.com>
2023-12-22 05:12:23 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const  REC_PER_PAGE = 10
+export const KEY = '0ed85beb-5f37-4c48-96fc-1b1bc5d19d99'
+export const MODAL_CLOSE_SEC = 2.5;
+
